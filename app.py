d4d1fdbc5393d2a7c967c2c731aa32959071889a
Piyush Jeena <56345075+pjeena@users.noreply.github.com>
2023-11-06 16:59:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ elif choose == "Customer modeling":
 
         else:
             st.warning(
-                "Enter OPENAI API key or URL/query regarding a product from Google Shopping",
+                "Enter OPENAI API key correctly or URL/query regarding a product from Google Shopping",
                 icon="⚠️",
             )
 
